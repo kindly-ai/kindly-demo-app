8c62c9327e0ce12672b345146401bfb22856fcd3
Gunnar Fornes <gunnarfornes@gmail.com>
2017-08-04 00:02:02 +0200

    style

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Demo' });
 });
 
 router.post('/kindly/webhooks', function(req, res) {
